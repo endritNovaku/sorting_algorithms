9985fc1b77a922a3e6bcacb80b8091aab2032ee1
endritNovaku <ennald@outlook.com>
2022-06-13 02:41:55 -0700

    fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,23 +6,22 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp;
+	size_t i, j, min, tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		tmp = array[i];
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-				tmp = array[j];
-			if (array[i] > tmp)
-			{
-				array[j] = array[i];
-				array[i] = tmp;
-			}
-
+			if (array[j] < array[min])
+				min = j;
 		}
-		print_array(array, size);
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
 	}
 }
